91e1e3402a65eb59e74ad2dd01f6bb22b0d2faa7
robin <goldensundd@hotmail.com>
2020-10-18 14:13:58 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# aigis_calculator
+ 
---
+++ src/main/java/com/jsyfer/calculator/AigisCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.jsyfer.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AigisCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AigisCalculatorApplication.class, args);
+	}
+
+}
